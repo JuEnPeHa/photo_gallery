4e9b5220ded80b44f3949d2633a1942f34492662
JuEnPeHa <j.enrique.23121996@gmail.com>
2023-03-27 12:05:51 -0600

    Olvide la parte de compartir

--- lib/pages/image_page.dart
+++ lib/pages/image_page.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:photo_gallery/main.dart';
 import 'package:photo_gallery/widgets/hero_image_widget.dart';
+import 'package:share_plus/share_plus.dart';
 
 class ImagePage extends StatelessWidget {
   final String title;
@@ -33,8 +36,27 @@ class ImagePage extends StatelessWidget {
           Text(description),
           Text(date.toString()),
           Text(tag),
+          MaterialButton(
+            color: Colors.deepOrangeAccent[100]!.withOpacity(0.5),
+            onPressed: () async => await shareImage(imageBase64),
+            child: Text('Share on Social Media'),
+          ),
+          SizedBox(
+            height: 20,
+          ),
         ],
       ),
     );
   }
+}
+
+Future<dynamic> shareImage(String imageBase64) async {
+  await Share.shareXFiles(
+    [
+      XFile.fromData(
+        base64Decode(imageBase64),
+        mimeType: 'image/png',
+      )
+    ],
+  );
 }
